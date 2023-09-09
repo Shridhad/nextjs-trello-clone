87b05f76b860fba4eb0909421f6adc0d8b4d36ec
Shridhar <shridhar.deshmukh3@gmail.com>
2023-09-09 02:08:01 +0200

    Prisma setup

--- app/login/messages.tsx
+++ app/login/messages.tsx
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 
 export default function Messages() {
-  const searchParams = useSearchParams()
-  const error = searchParams.get('error')
-  const message = searchParams.get('message')
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+  const message = searchParams.get("message");
   return (
     <>
       {error && (
-        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
+        <p className="mt-4 p-4 bg-neutral-900 text-orange-600 text-center">
           {error}
         </p>
       )}
@@ -19,5 +19,5 @@ export default function Messages() {
         </p>
       )}
     </>
-  )
+  );
 }
